7020f5ab4860137710f7842a516e1fe54df92df1
Santiago Anrango <santy2516@gmail.com>
2021-04-28 17:40:59 -0500

    Configuracion

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'host' => env('MAIL_HOST', 'smtp.google.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('santy2516@gmail.com'),
-            'password' => env('zdqmko2516crazygod'),
+            'username' => env('MAIL_USERNAME', 'santy2516@gmail.com'),
+            'password' => env('MAIL_PASSWORD', 'zdqmko2516crazygod'),
             'timeout' => null,
             'auth_mode' => null,
         ],
@@ -85,7 +85,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'santy2516@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'Api Mundos E'),
+        'name' => env('MAIL_FROM_NAME', "API Mundos E"),
     ],
 
     /*
